69c77c1890cfb10c1efe74ba9df2bd868dece496
snct-ukai <73654679+snct-ukai@users.noreply.github.com>
2021-12-08 22:26:35 +0900

    Update manage_fireflower.cpp

--- manage_fireflower.cpp
+++ manage_fireflower.cpp
@@ -1,9 +1,4 @@
 #include "manage_fireflower.h"
-#include <random>
-
-std::random_device Ran;
-std::mt19937 Mt(Ran());
-std::uniform_int_distribution<> rnd(0, 500);
 
 manage_fireflower::manage_fireflower(int num): num(num) {
 	particle = new manage_particle[this->num];
